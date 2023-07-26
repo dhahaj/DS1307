1c3f56f3336cfa03d114731cc1d3ef6c68c7e595
daniel <dhahaj@gmail.com>
2023-07-26 13:12:53 -0500

    add documentation.

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # DS1307 RTC Library
 
-This library is for the DS1307 RTC chip. It is a simple library that allows you to set and get the time from the RTC chip.
+This library provides a Python interface to the DS1307 real-time clock (RTC) chip over I2C. It uses the Telemetrix library to communicate with the RTC. The DS1307 RTC is a low-power, full binary-coded decimal (BCD) clock/calendar with 56 bytes of NV SRAM. Address and data are transferred serially through an I2C, bidirectional bus.
+
+## Installation
+
+To install the library, you can download the source code from this repository and then install it using pip:
+
+```
+pip install .
+```
+
+## Usage
+
+Here is an example of how to use the library:
+
+```python
+from ds1307 import DS1307
+
+# Initialize the RTC
+rtc = DS1307(port='COM3')
+
+# Set the time
+rtc.adjust(datetime(2022, 1, 1, 12, 0, 0))
+
+# Get the time
+print(rtc.get_date())
+```
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit a pull request.
--- ds1307.py
+++ ds1307.py
@@ -1,3 +1,14 @@
+"""
+This module provides a Python interface to the DS1307 real-time clock (RTC) over I2C.
+It uses the Telemetrix library to communicate with the RTC.
+
+The DS1307 RTC is a low-power, full binary-coded decimal (BCD) clock/calendar with 56 bytes of NV SRAM.
+Address and data are transferred serially through an I2C, bidirectional bus.
+
+This module provides methods to read and write the date and time from the RTC, as well as utility methods
+to convert between decimal and BCD (Binary-Coded Decimal) values.
+"""
+
 from telemetrix import telemetrix
 from datetime import datetime
 import time
@@ -9,6 +20,17 @@ class DS1307:
     DS1307 RTC class
 
     This class is used to interface with the DS1307 RTC.
+
+    Attributes:
+        DS1307_RAM_ADDR: The starting address of the DS1307's RAM.
+        DS1307_ADDRESS: The I2C address of the DS1307.
+        SEC_REG: The address of the seconds register.
+        MIN_REG: The address of the minutes register.
+        HOUR_REG: The address of the hours register.
+        DOW_REG: The address of the day of the week register.
+        DOM_REG: The address of the day of the month register.
+        MONTH_REG: The address of the month register.
+        YEAR_REG: The address of the year register.
     """
 
     DS1307_RAM_ADDR = 0x08
@@ -22,7 +44,12 @@ class DS1307:
     YEAR_REG = 0x06
 
     def __init__(self, port=None):
-        """Initialize the DS1307 RTC"""
+        """
+        Initialize the DS1307 RTC.
+
+        Args:
+            port (str, optional): The COM port to use for communication. Defaults to None.
+        """
         self.board = telemetrix.Telemetrix(com_port=port)
         self.board.set_pin_mode_i2c()
         time.sleep(0.1) 
@@ -31,15 +58,32 @@ class DS1307:
         self.start_time = None
 
     def get_date(self):
-        """Get the date from the RTC"""
+        """
+        Get the date from the RTC.
+
+        Returns:
+            datetime: The current date and time as a datetime object.
+        """
         return self.date
 
     def request_time(self):
-        """Get the time from the RTC"""
+        """
+        Get the time from the RTC.
+
+        This method sends a request to the RTC to read the time and date.
+        The actual time and date are processed in the process_data method.
+        """
         self.board.i2c_read(self.DS1307_ADDRESS, 0x00, 7, self.process_data)
 
     def write_time(self, hours, minutes, seconds):
-        """Write the time to the RTC"""
+        """
+        Write the time to the RTC.
+
+        Args:
+            hours (int): The hours to set, in 24-hour format.
+            minutes (int): The minutes to set.
+            seconds (int): The seconds to set.
+        """
         self.board.i2c_write(
             self.DS1307_ADDRESS, [self.SEC_REG, self.dec_to_bcd(seconds)]
         )
@@ -51,7 +95,14 @@ class DS1307:
         )
 
     def write_date(self, day, month, year):
-        """Write the date to the RTC"""
+        """
+        Write the date to the RTC.
+
+        Args:
+            day (int): The day of the month to set.
+            month (int): The month to set.
+            year (int): The year to set, in YYYY format.
+        """
         self.board.i2c_write(self.DS1307_ADDRESS, [self.DOM_REG, self.dec_to_bcd(day)])
         self.board.i2c_write(
             self.DS1307_ADDRESS, [self.MONTH_REG, self.dec_to_bcd(month)]
@@ -62,16 +113,41 @@ class DS1307:
         )
 
     def bcd_to_dec(self, bcd_val):
-        """Convert a BCD value to a decimal value"""
+        """
+        Convert a BCD value to a decimal value.
+
+        Args:
+            bcd_val (int): The BCD value to convert.
+
+        Returns:
+            int: The decimal equivalent of the BCD value.
+        """
         return ((bcd_val & 0xF0) >> 4) * 10 + (bcd_val & 0x0F)
 
     def dec_to_bcd(self, dec):
-        """Convert a decimal value to a BCD value"""
+        """
+        Convert a decimal value to a BCD value.
+
+        Args:
+            dec (int): The decimal value to convert.
+
+        Returns:
+            int: The BCD equivalent of the decimal value.
+        """
         tens, units = divmod(dec, 10)
         return (tens << 4) + units
 
     def process_data(self, data, time_stamp=None):
-        """Process the data returned from the RTC"""
+        """
+        Process the data returned from the RTC.
+
+        This method is called when data is received from the RTC.
+        It unpacks the data and updates the date attribute.
+
+        Args:
+            data (list): The data received from the RTC.
+            time_stamp (float, optional): The time the data was received. Defaults to None.
+        """
         # Unpack data
         (
             i2c_command,
@@ -104,7 +180,12 @@ class DS1307:
             self.start_time = self.date
 
     def adjust(self, dt):
-        """Adjust the RTC to the given datetime"""
+        """
+        Adjust the RTC to the given datetime.
+
+        Args:
+            dt (datetime): The datetime to set the RTC to.
+        """
         day = dt.day
         month = dt.month
         year = dt.year
@@ -116,5 +197,9 @@ class DS1307:
         self.write_time(hours, minutes, seconds)
 
     def close(self):
-        """Close the connection to the board"""
+        """
+        Close the connection to the board.
+
+        This method should be called when you are done using the RTC.
+        """
         self.board.shutdown()
